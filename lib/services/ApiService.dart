427c53217533aed6c771ddb4fa95940fd70e437f
Felipe11XZzx <153298467+Felipe11XZzx@users.noreply.github.com>
2025-03-04 18:37:01 +0100

    Implementado Actualizar Usuarios

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -9,26 +9,23 @@ class ApiService {
     receiveTimeout: const Duration(seconds: 10),
     contentType: 'application/json',
   ));
-  
+
   ApiService() {
-    _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) {
-        logger.d('Request: ${options.method} ${options.path}');
-        logger.d('Request Data: ${options.data}');
-        return handler.next(options);
-      },
-      onResponse: (response, handler) {
-        logger.d('Response: ${response.statusCode}');
-        logger.d('Response Data: ${response.data}');
-        return handler.next(response);
-      },
-      onError: (DioException e, handler) {
-        logger.e('API Error: ${e.response?.statusCode} ${_dio.options.baseUrl}${e.requestOptions.path}');
-        logger.e('Error Data: ${e.response?.data}');
-        logger.e('Error Message: ${e.message}');
-        return handler.next(e);
-      }
-    ));
+    _dio.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+      logger.d('Request: ${options.method} ${options.path}');
+      logger.d('Request Data: ${options.data}');
+      return handler.next(options);
+    }, onResponse: (response, handler) {
+      logger.d('Response: ${response.statusCode}');
+      logger.d('Response Data: ${response.data}');
+      return handler.next(response);
+    }, onError: (DioException e, handler) {
+      logger.e(
+          'API Error: ${e.response?.statusCode} ${_dio.options.baseUrl}${e.requestOptions.path}');
+      logger.e('Error Data: ${e.response?.data}');
+      logger.e('Error Message: ${e.message}');
+      return handler.next(e);
+    }));
   }
 
   Dio get dio => _dio;
